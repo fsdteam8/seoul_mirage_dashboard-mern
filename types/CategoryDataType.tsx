63affdff2e8fc3d6f3583597731337a3758975cb
mehedi hasan shishir <mehedihasanshishir.info@gmail.com>
2025-06-15 11:49:45 +0600

    added

--- types/CategoryDataType.tsx
+++ types/CategoryDataType.tsx
@@ -1,12 +1,12 @@
 export type Category = {
-  id: string;
+  id: number; // updated from string to number
   name: string;
   description: string;
   type: string;
+  image: string; // required now
   created_at: string;
   updated_at: string;
-  imageUrl?:string
-  productCount?:number
+  // removed productCount as it's not in new data
 };
 
 export type PaginationLink = {
